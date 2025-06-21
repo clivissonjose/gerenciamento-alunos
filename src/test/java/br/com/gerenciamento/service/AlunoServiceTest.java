d73315af24d3a46047ac4d4f37be2c66370bff25
clivissonjose <cjbarbosa2001@gmail.com>
2025-06-20 21:10:26 -0300

    testes unitários de alunoServiceTest

--- src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/AlunoServiceTest.java
@@ -11,6 +11,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class AlunoServiceTest {
@@ -30,7 +37,7 @@ public class AlunoServiceTest {
         this.serviceAluno.save(aluno);
 
         Aluno alunoRetorno = this.serviceAluno.getById(1L);
-        Assert.assertTrue(alunoRetorno.getNome().equals("Vinicius"));
+        assertTrue(alunoRetorno.getNome().equals("Vinicius"));
     }
 
     @Test
@@ -44,4 +51,98 @@ public class AlunoServiceTest {
         Assert.assertThrows(ConstraintViolationException.class, () -> {
                 this.serviceAluno.save(aluno);});
     }
+
+    // NOVOS TESTES
+
+    // Teste 1: Deletar um usuário inexistente
+    @Test
+    public void deletarAluno(){
+
+        Aluno aluno = new Aluno();
+        aluno.setNome("Joao Santos de Araujo Lima");
+        aluno.setId(1L);
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("123456");
+
+        serviceAluno.save(aluno);
+        serviceAluno.deleteById(aluno.getId());
+
+        Assert.assertThrows(NoSuchElementException.class, () -> {
+
+            this.serviceAluno.getById(1L) ;
+
+        });
+
+    }
+
+    // Teste 2: criar um aluno sem número da matrícula
+
+    @Test
+    public void criarAlunoSemMatricula(){
+
+        Aluno aluno = new Aluno();
+        aluno.setNome("Joao Santos de Araujo Lima");
+        aluno.setId(1L);
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+
+        Assert.assertThrows(ConstraintViolationException.class, () -> {
+            this.serviceAluno.save(aluno);});
+
+    }
+
+    // teste 3: Atualizar cadastro de aluno
+    @Test
+    public void atualizarAluno() {
+
+        Aluno aluno = new Aluno();
+
+        aluno.setNome("Carolina De Araújo");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("7875655578");
+
+        this.serviceAluno.save(aluno);
+
+        Aluno alunoSalvo = this.serviceAluno.getById(aluno.getId());
+
+        Assert.assertNotNull(alunoSalvo);
+
+        alunoSalvo.setNome("Maria Aparecida");
+        alunoSalvo.setCurso(Curso.DIREITO);
+        this.serviceAluno.save(alunoSalvo);
+
+        Aluno alunoAtualizado = this.serviceAluno.getById(alunoSalvo.getId());
+        assertEquals("Maria Aparecida", alunoAtualizado.getNome());
+        assertEquals(Curso.DIREITO, alunoAtualizado.getCurso());
+    }
+
+
+    // teste 4: Testar busca por nome, em qualquer formato
+
+    @Test
+    public void findByNomeContainingIgnoreCaseTest(){
+
+        Aluno aluno = new Aluno();
+
+        aluno.setNome("CaRoLiNa De AraÚJO");
+        aluno.setTurno(Turno.NOTURNO);
+        aluno.setCurso(Curso.ADMINISTRACAO);
+        aluno.setStatus(Status.ATIVO);
+        aluno.setMatricula("7875655578");
+
+        this.serviceAluno.save(aluno);
+
+        List<Aluno> resultado = serviceAluno.findByNomeContainingIgnoreCase("CAROLINA DE ARAÚJO");
+
+        assertEquals(1, resultado.size());
+                     //CaRoLiNa De AraÚJO
+        assertEquals(aluno.getNome(), resultado.get(0).getNome());
+
+    }
+
 }
