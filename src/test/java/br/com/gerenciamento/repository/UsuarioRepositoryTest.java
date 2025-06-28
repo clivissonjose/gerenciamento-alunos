5f7c8d00337ccc96770b242f35bac30ebdd360f3
clivissonjose <cjbarbosa2001@gmail.com>
2025-06-28 18:55:26 -0300

    4 testes unitários de UsuarioRepository

--- src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
+++ src/test/java/br/com/gerenciamento/repository/UsuarioRepositoryTest.java
@@ -1,10 +1,96 @@
 package br.com.gerenciamento.repository;
 
+import br.com.gerenciamento.model.Usuario;
+import jakarta.validation.ConstraintViolationException;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertTrue;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.test.annotation.Rollback;
+
+@Transactional
+@Rollback
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UsuarioRepositoryTest {
+ /*
+    @Query("select e from Usuario e where e.email = :email")
+    Usuario findByEmail(String email);
+
+    @Query("select l from Usuario l where l.user = :user and l.senha = :senha")
+    Usuario buscarLogin(String user, String senha); */
+
+    @Autowired
+    UsuarioRepository usuarioRepository;
+
+    @Test
+    public void findByEmailTest(){
+
+        Usuario usuario = new Usuario();
+        usuario.setEmail("emailtest@gmial.com");
+        usuario.setSenha("r637362");
+        usuario.setUser("Aline Gomes");
+        usuario.setId(1L);
+
+        this.usuarioRepository.save(usuario);
+
+        Usuario usuarioRetornado = this.usuarioRepository.findByEmail("emailtest@gmial.com");
+        assertTrue(usuario.getEmail().equals(usuarioRetornado.getEmail()));
+        assertTrue(usuarioRetornado.getId().equals(1L));
+    }
+
+    @Test
+    public void buscarLoginTest(){
+
+        Usuario usuario = new Usuario();
+        usuario.setEmail("emailtest@gmial.com");
+        usuario.setSenha("r637362");
+        usuario.setUser("Aline Gomes");
+        usuario.setId(1L);
+
+        this.usuarioRepository.save(usuario);
+
+        Usuario usuarioRetornado = this.usuarioRepository.buscarLogin("Aline Gomes", "r637362");
+        assertTrue(usuarioRetornado.getUser().equals("Aline Gomes"));
+        assertTrue(usuarioRetornado.getSenha().equals("r637362"));
+
+    }
+
+
+    @Test
+    public void usuarioComNomeCurto(){
+
+        Usuario usuario = new Usuario();
+
+        usuario.setUser("ab");
+        usuario.setSenha("Neyeyeyey");
+        usuario.setEmail("email@gmail.com");
+
+        //this.usuarioRepository.save(usuario);
+        Assert.assertThrows(ConstraintViolationException.class, () -> {
+            this.usuarioRepository.save(usuario);});
+
+    }
+
+ //
+    @Test
+    public void usuarioComNomeMuitoLongo(){
+
+        Usuario usuario = new Usuario();
+
+        usuario.setUser("Nome com mais de 20 caracteres ");
+        usuario.setSenha("Neyeyeyey");
+        usuario.setEmail("email@.com");
+
+        Assert.assertThrows(ConstraintViolationException.class, () -> {
+            this.usuarioRepository.save(usuario);});
+
+    }
+
 }
